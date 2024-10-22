cf3983411cf7a75a4e377493932002b635136845
Lakshya-2001 <86869227+Lakshya-2001@users.noreply.github.com>
2024-10-22 22:19:34 +0530

    Initial commit

---
+++ ZoomLayout/build.gradle.kts
@@ -0,0 +1,72 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+         alias(libs.plugins.android.library)
+         alias(libs.plugins.jetbrains.kotlin.android)
+         id("maven-publish")
+}
+
+android {
+         namespace = "com.lakshya.zoomlayout"
+         compileSdk = 34
+
+         defaultConfig {
+                  minSdk = 24
+
+                  testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+                  consumerProguardFiles("consumer-rules.pro")
+         }
+
+         buildTypes {
+                  release {
+                           isMinifyEnabled = false
+                           proguardFiles(
+                                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                                    "proguard-rules.pro"
+                           )
+                  }
+         }
+         compileOptions {
+                  sourceCompatibility = JavaVersion.VERSION_1_8
+                  targetCompatibility = JavaVersion.VERSION_1_8
+         }
+         kotlinOptions {
+                  jvmTarget = "1.8"
+         }
+}
+
+
+publishing {
+         publications {
+                  register<MavenPublication>("release") {
+                           groupId = "com.github.Lakshya-2001"
+                           artifactId = "zoomlayoutpro"
+                           version = "1.0.1"
+
+                           afterEvaluate {
+                                    from(components["release"])
+                           }
+                  }
+         }
+         repositories {
+                  maven {
+                           name = "zoomlayoutpro"
+                           url = uri("https://maven.pkg.github.com/Lakshya-2001/zoomlayoutpro")
+                           credentials {
+//                                    username = (project.findProperty("gpr.user") ?: System.getenv("GITHUB_USERNAME")).toString()
+//                                    password = (project.findProperty("gpr.token") ?: System.getenv("GITHUB_TOKEN")).toString()
+
+                           }
+                  }
+         }
+}
+dependencies {
+         implementation(libs.egloo)
+         implementation(libs.androidx.core.ktx)
+         implementation(libs.androidx.appcompat)
+         implementation(libs.material)
+         testImplementation(libs.junit)
+         androidTestImplementation(libs.androidx.junit)
+         androidTestImplementation(libs.androidx.espresso.core)
+}
